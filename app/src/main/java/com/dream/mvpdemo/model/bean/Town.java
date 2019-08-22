97047ee639762044a7ec1dd6c3350104e09207e4
HandsomeNuoNuo <54341363+HandsomeNuoNuo@users.noreply.github.com>
2019-08-22 09:18:06 +0800

    second commit

---
+++ app/src/main/java/com/dream/mvpdemo/model/bean/Town.java
@@ -0,0 +1,13 @@
+package com.dream.mvpdemo.model.bean;
+
+/**********************************
+ * @Name: Town
+ * @Copyright： 个人版权所有
+ * @CreateDate： 2019/8/22 9:17
+ * @author: HuangFeng
+ * @Version： 1.0
+ * @Describe:
+ *
+ **********************************/
+public class Town {
+}
